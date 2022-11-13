d59192a739ab8c82dfe33b368cc3299dd075cade
MrBElga <43758059+MrBElga@users.noreply.github.com>
2022-11-12 23:31:09 -0300

    TAD inserir

--- pre-alpha/listas.h
+++ pre-alpha/listas.h
@@ -1,3 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "listas.h"
+#include <ctype.h>
+#include <windows.h>
+#include <conio2.h>
+#include <time.h>
+#include <string.h>
+
+
 // ##################################################
 // # STRUCTS E FUNCOES PARA MANIPULACAO DAS LISTAS  #
 // ##################################################
@@ -9,7 +19,7 @@ struct TpNotas{
     TpNotas *prox;
 };
 struct TpAlunos{
-    char Curso[30], Rua[30], Bairro[30], Cidade[30], Estado[25];
+    char Nome[30],Curso[30], Rua[30], Bairro[30], Cidade[30], Estado[25];
     int Endereco,Dia,Mes,Ano;
     TpNotas *ListaNotas;
     TpAlunos *Ant,*Prox;
@@ -19,16 +29,77 @@ struct TpDescritorA{
     int Qtde;
     TpAlunos *Inicio, *Fim;
 };
+
 void IniciarDescA(TpDescritorA &DescA);
 
 //INICIO DOS DESCRITORES
-
 void IniciarDescA(TpDescritorA &DescA){
     DescA.Inicio = DescA.Fim = NULL;
     DescA.Qtde = 0;
 }
 
 //ALUNOS
+TpAlunos *NovaCaixa(char Nome[]){
+	TpAlunos *Caixa = new TpAlunos;
+    char aux[30];
 
-    
+	strcpy(Caixa->Nome, Nome[30]);
+    printf("\nDigite o Curso: ");
+    gets(aux);
+    strcpy(Caixa->Curso,aux);
+    printf("\nDigite a Cidade: ");
+    gets(aux);
+    strcpy(Caixa->Cidade,aux);
+    printf("\nDigite o Bairro");
+    gets(aux);
+    strcpy(Caixa->Bairro,aux);
+    printf("\nDigite a Rua: ");
+    gets(aux);
+    strcpy(Caixa->Rua,aux);
+    printf("\nDigite o Estado: ");
+    gets(aux);
+    strcpy(Caixa->Estado,aux);
+    printf("\nDigite o Endereco: ");
+    gets(aux);
+    strcpy(Caixa->Endereco,aux);
+    printf("\nDigite o Ano");
+
+    Caixa->Prox = Caixa->Ant = NULL;
+	return Caixa;  
+}
+
+TpAlunos *AdcionarAlunos(TpAlunos *Lista,char Nome[]){
+    TpAlunos *NC = NovaCaixa(Nome), *ant , *atual;
+	Desc.Qtde++;
+	if(Desc.Inicio==NULL){
+		Desc.Inicio=Desc.Fim=NC;
+	}
+	else{
+		if(Desc.Inicio->Info >= Elem){
+			NC->prox = Desc.Inicio;
+			Desc.Inicio = NC;
+		}
+		else{
+			if(Desc.Fim->Info <= Elem){
+				Desc.Fim->prox = NC;
+				Desc.Fim = NC;
+			}
+			else{
+				atual = Desc.Inicio;
+				atual = Desc.Inicio->prox;
+				
+				while(atual->Info < Elem){
+					ant = atual;
+					atual = atual->prox;
+				}
+					
+				NC->prox = atual;
+				ant->prox = NC;
+			}
+		}
+	}
+    return Lista;
+}
+
+
 //NOTAS
